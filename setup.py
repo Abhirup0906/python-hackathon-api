50f2e50a8c960cd851a8ecda2daac8e9a7a73c4b
Abhirup Sadhukhan <abhirupsadhukhan@gmail.com>
2024-03-16 17:08:34 +0530

    deleted unnecesssary files

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-from setuptools import setup, find_packages
-
-setup(
-    name='team85-voice-recognition',
-    description='Voice Recognition',
-    packages=find_packages(),
-    version=os.environ.get('BUILDNUMBER')
-)
